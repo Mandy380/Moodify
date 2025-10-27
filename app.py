93c2b868e1ab2ef7e584e8d020f64b5b2cf281fa
Mandy380 <72581127+Mandy380@users.noreply.github.com>
2025-10-28 01:35:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template, request
+import json, random
 
+app = Flask(__name__)
+
+with open('moods.json', 'r') as f:
+  mood_d = json.load(f)
+
+@app.route('/')
+def home():
+  return render_template('inder.html')
+
+@pp.route('/results', methods=['POST'])
