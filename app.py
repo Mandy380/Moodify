deac6c5dd71e0a24bd098a6a65ed8787ea65e415
Mandy380 <72581127+Mandy380@users.noreply.github.com>
2025-10-29 01:00:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ with open('moods.json', 'r') as f:
 
 @app.route('/')
 def home():
-  return render_template('inder.html')
+  return render_template('index.html')
 
 @pp.route('/results', methods=['POST'])
+def result():
+  mood = request.form['mood']
+  quotes = mood_d.get(mood,"Stay Blessed and Be You, Always!"])
+  quote = random.choice(quotes)
+  return render_template('result.html', mood = mood, quote = quote)
+
+if __name__ == '__main__':
+  app.run(debug=True)
